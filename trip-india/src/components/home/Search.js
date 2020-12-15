2a1267e9f8991c9dbdba0025563e530a91b42567
Mukesh Maurya <mukesh.india9@gmail.com>
2020-12-15 22:44:34 +0530

    adding hotel selction feature wrt location

--- trip-india/src/components/home/Search.js
+++ trip-india/src/components/home/Search.js
@@ -1,17 +1,84 @@
 import React, { Component } from 'react';
 import './Search.css';
 
-class Search extends Component{
-    render(){
-        return(
+const url = 'https://developerfunnel.herokuapp.com/location';
+const hotalURL = "https://developerfunnel.herokuapp.com/hotels?city="
+class Search extends Component {
+    constructor() {
+        super()
+        this.state = {
+            location: "",
+            hotal: ""
+        }
+    }
+
+    renderCity = (data) => {
+        if (data) {
+            return data.map((item) => {
+                return (
+                    <option value={item.city}>
+                        {item.city_name}
+                    </option>
+                )
+            });
+        }
+    };
+
+    changeHandler = (event) => {
+        const hotelKey = event.target.value;
+        fetch(`${hotalURL}${hotelKey}`, { method: 'GET' })
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({ hotal:data })
+            })
+            .catch((error) => {
+                console.log("Data not Found")
+            })
+    }
+
+
+    renderHotal = (data) => {
+        if (data) {
+            return data.map((item) => {
+                return (
+                    <option value={item._id}>
+                        {item.name} | {item.locality}
+                    </option>
+                )
+            })
+        }
+    }
+
+    render() {
+        return (
             <div class="imageContainer">
                 <div id="logo">
                     D!
                 </div>
-                <h1>Search</h1>
+                <div className="heading">Plan your trip</div>
+                <div className="locationSelector">
+                    <select className="locationDropDown" onChange={this.changeHandler}>
+                        <option>---- Select Location ----</option>
+                        {this.renderCity(this.state.location)}
+                    </select>
+                    <select className="reataurantsinput" >
+                        <option>---- Select Hotal ----</option>
+                        {this.renderHotal(this.state.hotal)}
+                    </select>
+                </div>
             </div>
         )
     }
-};
 
+    componentDidMount() {
+        fetch(url, { method: 'GET' })
+            .then((resp) => resp.json())
+            .then((data) => {
+                this.setState({ location: data })
+            })
+            .catch((error) => {
+                console.log("Data not Found")
+            })
+    }
+};
 export default Search;
